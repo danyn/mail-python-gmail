7395f49b8d07176e8a1acedd2ba63b28eef7d37d
danyn <danyn.com@gmail.com>
2018-06-11 16:49:05 -0700

    Serve on https and Set the CORS headers also reply with json and send a 201

--- mail-server-http-server.py
+++ mail-server-http-server.py
@@ -7,6 +7,8 @@ import cgi
 import os
 import threading
 from socketserver import ThreadingMixIn
+import json
+import ssl
 
 # turn up amount of excepions
 # import cgitb
@@ -28,30 +30,54 @@ class ThreadHTTPServer (ThreadingMixIn, http.server.HTTPServer):
 class EmailResponder(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
-        self.send_response(200)
-        #  self.send_header('content-type','application/json')
-        self.end_headers()
 
         ctype, pdict = cgi.parse_header(self.headers['content-type'])
         pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
 
         if ctype == 'multipart/form-data':
+            self.send_response(201)
+            self.send_header('Content-Type','application/json')
+            self.send_header('Access-Control-Allow-Credentials','true')
+            self.send_header('Access-Control-Allow-Origin','http://localhost:4000')
+            self.send_header('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
+            self.send_header('AMP-Access-Control-Allow-Source-Origin', 'http://localhost:4000')
+            self.end_headers()
+
+            # get useful fields from the request 
             fields = cgi.parse_multipart(self.rfile, pdict)
-            first_name = fields.get('First_Name')
-            last_name = fields.get('Last_Name')
-            email_address = fields.get('Email_Address')
-            telephone_number = fields.get('Telephone_Number')
+            first_name = fields.get('First_Name')[0].decode()
+            last_name = fields.get('Last_Name')[0].decode()
+            email_address = fields.get('Email_Address')[0].decode()
+            telephone_number = fields.get('Telephone_Number')[0].decode()
+            json_response = json.dumps({'email_address': email_address,'first_name':first_name, 'last_name':last_name})
+            self.wfile.write(bytes(json_response, 'utf-8'))
+
+
+
+            # self.send_header('','')
+            # self.send_header('','')
+            # for request on same domain as source
+   
+            # Access-Control-Allow-Credentials: true
+            # Access-Control-Allow-Origin: https://example.com
+            # Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin
+            # AMP-Access-Control-Allow-Source-Origin: https://example.com
+
+           
+
+
 
             # print(first_name + last_name + email_address + telephone_number)
-            print(first_name[0].decode() + last_name[0].decode() + email_address[0].decode() + telephone_number[0].decode() )
+            # print(first_name[0].decode() + last_name[0].decode() + email_address[0].decode() + telephone_number[0].decode() )
 
-
+            print(first_name)
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',8000))
     server_address = ('', port)
     httpd = ThreadHTTPServer(server_address, EmailResponder)
+    httpd.socket=ssl.wrap_socket(httpd.socket, certfile = '/Users/dan/.certs/server.pem', server_side=True)
     httpd.serve_forever()
 
 
